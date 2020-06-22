672f55a4989382828e98a3fd3a47ee2ebe36177a
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 16:24:59 +0300

    return @pages alias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Home, Contacts } from './pages';
+import { Home, Contacts } from '@pages';
 
 const App = () => (
   <Switch>

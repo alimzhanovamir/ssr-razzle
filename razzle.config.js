a8df95327725db753c0adeb844516cffc7c67761
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 14:39:44 +0300

    Revert "remove old code"
    This reverts commit d0f880dea81e383fb7f65cbd053ab46fbef3b3ee.

--- razzle.config.js
+++ razzle.config.js
@@ -1,8 +1,12 @@
+const isHeroku = require("is-heroku")
+const entries = require("object.entries")
 const path = require('path');
 
 module.exports = {
   modify(config, { target, dev }, webpack) {
     const appConfig = config;
+
+    
 
     appConfig.resolve = {
       ...appConfig.resolve,
@@ -12,6 +16,26 @@ module.exports = {
         '@ui': path.resolve(__dirname, 'src/ui'),
       } 
     };
+
+    if (target !== "node") return appConfig
+
+    const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
+    const indexDefinePlugin = appConfig.plugins.findIndex(isDefinePlugin)
+  
+    if (indexDefinePlugin < 0) {
+      console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
+      return appConfig
+    }
+  
+    const {definitions} = appConfig.plugins[indexDefinePlugin]
+    const newDefs = Object.assign({}, definitions);
+  
+    if (isHeroku) {
+      delete newDefs["process.env.PORT"]
+      newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
+    }
+  
+    appConfig.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
 
     return appConfig;
   }

d0a6a8d813990360865f41ba248d86273050dc7e
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 15:52:02 +0300

    test

--- razzle.config.js
+++ razzle.config.js
@@ -1,25 +1,11 @@
 const razzleHeroku = require('razzle-heroku');
 const path = require('path');
-const razzleConfigEnv = require('razzle/config/env');
 
 module.exports = {
   modify: (config, { target, dev }, webpack) => {
-    // const appConfig = razzleHeroku(config, { target, dev }, webpack);
-
-    if (target === 'node') {
-      config.plugins = config.plugins.filter(plugin => plugin.constructor.name !== 'DefinePlugin');
-      const dotenv = razzleConfigEnv.getClientEnv(target, {
-        clearConsole: true,
-        host: 'localhost',
-        port: 3000
-      });
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          'process.env': `Object.assign(${JSON.stringify(dotenv.raw)}, process.env)`
-        })
-      );
-    }
-
+    config = razzleHeroku(config, { target, dev }, webpack);
+    console.log(config);
+    
     config.resolve = {
       ...config.resolve,
       alias: {

df48996e96bbdce5376cfd9254d6210b57e857f6
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 16:22:43 +0300

    test

--- razzle.config.js
+++ razzle.config.js
@@ -1,23 +1,23 @@
-// const razzleHeroku = require('razzle-heroku');
-// const path = require('path');
-
-// module.exports = {
-//   modify: (config, { target, dev }, webpack) => {
-//     config = razzleHeroku(config, { target, dev }, webpack);
-//     console.log(config);
-    
-//     config.resolve = {
-//       ...config.resolve,
-//       alias: {
-//         '@pages': path.resolve(__dirname, 'src/pages'),
-//         '@features': path.resolve(__dirname, 'src/features'),
-//         '@ui': path.resolve(__dirname, 'src/ui'),
-//       } 
-//     };
-//     return config;
-//   }
-// };
+const razzleHeroku = require('razzle-heroku');
+const path = require('path');
 
 module.exports = {
-  modify: require("razzle-heroku")
-}
+  modify: (config, { target, dev }, webpack) => {
+    config = razzleHeroku(config, { target, dev }, webpack);
+    console.log(config);
+    
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@features': path.resolve(__dirname, 'src/features'),
+        '@ui': path.resolve(__dirname, 'src/ui'),
+      } 
+    };
+    return config;
+  }
+};
+
+// module.exports = {
+//   modify: require("razzle-heroku")
+// }

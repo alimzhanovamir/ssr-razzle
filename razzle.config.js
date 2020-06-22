ce5b1cd5bff26c7348135d4355e8190aa11fb497
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 15:33:16 +0300

    test

--- razzle.config.js
+++ razzle.config.js
@@ -1,36 +1,10 @@
-const isHeroku = require("is-heroku")
-const entries = require("object.entries")
+const razzleHeroku = require('razzle-heroku');
 const path = require('path');
 
 module.exports = {
   modify(config, { target, dev }, webpack) {
-    const appConfig = config;
+    const appConfig = razzleHeroku(config, { target, dev }, webpack);
 
-    if (target !== "node") return appConfig
-    console.log(' LOG 1');
-    
-    const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
-    const indexDefinePlugin = appConfig.plugins.findIndex(isDefinePlugin)
-    
-    if (indexDefinePlugin < 0) {
-      console.log(' LOG 2');
-      console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
-      return appConfig
-    }
-    
-    const {definitions} = appConfig.plugins[indexDefinePlugin]
-    const newDefs = Object.assign({}, definitions);
-    
-    if (isHeroku) {
-      console.log(' LOG 3');
-      delete newDefs["process.env.PORT"]
-      newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
-    }
-    
-    console.log(' LOG 4');
-    appConfig.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
-
-    
     appConfig.resolve = {
       ...appConfig.resolve,
       alias: {
@@ -39,7 +13,6 @@ module.exports = {
         '@ui': path.resolve(__dirname, 'src/ui'),
       } 
     };
-
     return appConfig;
   }
 };

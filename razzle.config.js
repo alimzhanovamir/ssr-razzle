27e7d32db4cc6311a421295447e9cbc7b99595cc
Alimzhanov Amir <alimzhanovamir@mail.ru>
2020-06-22 15:28:57 +0300

    test

--- razzle.config.js
+++ razzle.config.js
@@ -6,8 +6,31 @@ module.exports = {
   modify(config, { target, dev }, webpack) {
     const appConfig = config;
 
+    if (target !== "node") return appConfig
+    console.log(' LOG 1');
     
+    const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
+    const indexDefinePlugin = appConfig.plugins.findIndex(isDefinePlugin)
+    
+    if (indexDefinePlugin < 0) {
+      console.log(' LOG 2');
+      console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
+      return appConfig
+    }
+    
+    const {definitions} = appConfig.plugins[indexDefinePlugin]
+    const newDefs = Object.assign({}, definitions);
+    
+    if (isHeroku) {
+      console.log(' LOG 3');
+      delete newDefs["process.env.PORT"]
+      newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
+    }
+    
+    console.log(' LOG 4');
+    appConfig.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
 
+    
     appConfig.resolve = {
       ...appConfig.resolve,
       alias: {
@@ -16,26 +39,6 @@ module.exports = {
         '@ui': path.resolve(__dirname, 'src/ui'),
       } 
     };
-
-    if (target !== "node") return appConfig
-
-    const isDefinePlugin = plugin => plugin.constructor.name === "DefinePlugin"
-    const indexDefinePlugin = appConfig.plugins.findIndex(isDefinePlugin)
-  
-    if (indexDefinePlugin < 0) {
-      console.warn("Couldn't setup razzle-heroku, no DefinePlugin...")
-      return appConfig
-    }
-  
-    const {definitions} = appConfig.plugins[indexDefinePlugin]
-    const newDefs = Object.assign({}, definitions);
-  
-    if (isHeroku) {
-      delete newDefs["process.env.PORT"]
-      newDefs["process.env.RAZZLE_PUBLIC_DIR"] = '"/app/build/public"'
-    }
-  
-    appConfig.plugins[indexDefinePlugin] = new webpack.DefinePlugin(newDefs)
 
     return appConfig;
   }
